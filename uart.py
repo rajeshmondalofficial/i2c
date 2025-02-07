1c6364cb71f4dc2ed17ebf8fe437272b6ef82927
Rajesh Mondal <rajeshmondal9007@gmail.com>
2025-02-08 00:07:02 +0530

    sucs

--- uart.py
+++ uart.py
@@ -12,16 +12,13 @@ uart = serial.Serial(
     timeout=1             # Timeout in seconds for reading
 )
 
-uart.write(b'Hello From Pi3')
-
-
-# try:
-#     print("Reading from UART...")
-#     while True:
-#         if uart.in_waiting > 0:  # Check if data is available
-#             data = uart.readline().decode('utf-8').strip()  # Read and decode
-#             print(f"Received: {data}")
-# except KeyboardInterrupt:
-#     print("Exiting...")
-# finally:
-#     uart.close()
+try:
+    print("Reading from UART...")
+    while True:
+        if uart.in_waiting > 0:  # Check if data is available
+            data = uart.readline().decode('utf-8').strip()  # Read and decode
+            print(f"Received: {data}")
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    uart.close()
